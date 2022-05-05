44fbf31c09e6b40121f6888f99ae414f96a703dd
VenkataJaswanth <85697901+VenkataJaswanth@users.noreply.github.com>
2022-05-05 18:26:27 +0530

    Add files via upload

---
+++ Frontend/interface.js
@@ -0,0 +1,9 @@
+// interface interface_name {  
+//     // variables' declaration  
+//     // methods' declaration  
+// }  
+var empObject = {};
+empObject.name = "Manikanta";
+empObject.age = 22;
+console.log("Name: " + empObject.name);
+console.log("Age: " + empObject.age);
